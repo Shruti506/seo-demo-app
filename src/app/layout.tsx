550c1b31d3c7362ae5ea0f2cce344c27535d50fd
Shruti <shruti.dhameliya@dignizant.com>
2025-11-24 10:00:29 +0530

    Add missing head tag in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
